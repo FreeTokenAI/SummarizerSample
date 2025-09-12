f573d7b8d78a60d36cc8e29d0e0ece35d10fd897
Vince Francesi <vincefrancesi@me.com>
2025-09-11 17:52:23 -0700

    Initial Commit

---
+++ Summarizer/SummarizerApp.swift
@@ -0,0 +1,17 @@
+//
+//  SummarizerApp.swift
+//  Summarizer
+//
+//  Created by Vince Francesi on 9/11/25.
+//
+
+import SwiftUI
+
+@main
+struct SummarizerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
